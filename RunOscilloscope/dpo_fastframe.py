73ac00abe79e44441820b8829c24a7a206f63fe5
tommasoisi <tommasoisi@gmail.com>
2018-11-09 14:25:19 -0600

    Update dpo_fastframe.py

--- RunOscilloscope/dpo_fastframe.py
+++ RunOscilloscope/dpo_fastframe.py
@@ -68,10 +68,8 @@ def copytree(src, dst, symlinks=False, ignore=None):
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
-        if os.path.isdir(s):
-            shutil.copytree(s, d, symlinks, ignore)
-        else:
-            shutil.copy2(s, d)
+        shutil.copytree(item, d, symlinks, ignore)
+
 
 
 """#################SEARCH/CONNECT#################"""
