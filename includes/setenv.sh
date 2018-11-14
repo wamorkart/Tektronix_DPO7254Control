21b7e17cacabff207cacc74f74f76b7359553820
Nicola Minafra <nicola.minafra@cern.ch>
2018-11-14 18:06:38 +0100

    added setenv for lxplus

---
+++ includes/setenv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export FNAL_SCOPE_DIR=/eos/cms/store/group/phys_susy/razor/FNAL_TB_1811/Scope_standalone/
+. /cvmfs/sft.cern.ch/lcg/releases/LCG_88/ROOT/6.08.06/x86_64-slc6-gcc62-opt/ROOT-env.sh
